7f130569cd44d9b5a8abbed2087194b2a087e823
VanoKi <kirsvano@gmail.com>
2023-10-08 16:56:43 +0300

    feat: add start stop

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const countDownEl = document.getElementById('countdown')
 let time = 0
+    timeRun = false
+    intervalID = 0;
 
 // setInterval(updateCounDown, 1000)
 
-function updateCounDown(){
+function updateCountDown(){
   const minutes = Math.floor(time / 60)
   let seconds = time % 60;
   seconds = seconds < 10 ? `0${seconds}` :seconds
@@ -11,5 +13,17 @@ function updateCounDown(){
   time++
 }
 
+function start(){
+  if (!timeRun) {
+    clearInterval(intervalID)
+    time = 0
+    intervalID = setInterval(updateCountDown, 1000)
+    timeRun = true
+  }else{
+    clearInterval(intervalID)
+    timeRun = false
+  }
+}
+
 const btn = document.querySelector('.start')
-btn.addEventListener('click', setInterval(updateCounDown, 1000))
+btn.addEventListener('click', start)
